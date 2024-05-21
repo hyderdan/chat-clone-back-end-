c876a0e13dd659edd65c187428d9589b812b20ef
Hyder Danish <hyder.danish369@gmail.com>
2024-05-21 16:36:39 +0530

    done

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const {CONNECT} = require('./config')
+const {CONNECT} = require('./config');
+const {userROutes} = require('./routes/userRoutes')
 const app = express();
 const PORT = 3000;
 CONNECT();
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:4200",
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true
     }
 ));
 
+app.use('/users',userROutes);
 
 app.listen(PORT, () => {
     console.log('server starts at port',PORT)
