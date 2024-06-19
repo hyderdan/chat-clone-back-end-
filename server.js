a96ce7f903867791feb94e6c96f4f2691b5ffec1
Hyder Danish <hyder.danish369@gmail.com>
2024-06-19 17:48:10 +0530

    done

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const {CONNECT} = require('./config');
 const {userROutes} = require('./routes/userRoutes');
-const sockectIO = require('socket.io');
-const server = http.Server(app);
-const Io = sockectIO(server)
-const http = require('http');
+const admin = require('firebase-admin');
+const serviceAccount = require('./chat-application-6f43e-firebase-adminsdk-yg075-7efb60a401.json');
 const app = express();
 const PORT = 3000;
 CONNECT();
@@ -19,29 +17,38 @@ app.use(cors(
         credentials: true
     }
 ));
-
 app.use('/users',userROutes);
 
-const io = socketIo(server, {
-    cors: {
-        origin: "http://localhost:4200",
-        methods: ["GET", "POST"]
+admin.initializeApp({
+    credential:
+    admin.credential.cert(serviceAccount) , 
+    databaseURL:
+    "https://chat-application-6f43e-default-rtdb.firebaseio.com"
+});
+const db =admin.database();
+
+app.post('/send-mes', async (req, res) => {
+    try {
+        const { username, message } = req.body;
+        const messageData = { username, message, Timestamp: Date.now() };
+        db.ref('messages').push(messageData)
+        res.status(200).json('message send successfullly')
+    } catch (err) {
+        console.log(err); 
     }
 });
+app.get('/get-mes', async(req,res)=>{
+    try{
+        const messageRef = db.ref('message');
+        messageRef.once('value',(snapshot)=>{
+            const messages = snapshot.val();
+            res.status(200).json(messages);
+        })
+    }catch(err){
+        console.log(err);
+    }
+})
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // Example: Send a HTML file for the root route
-});
-
-io.on('connection', (client) => {
-    console.log('a user connected');
-    client.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-    client.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
 
 app.listen(PORT, () => {
     console.log('server starts at port',PORT)
