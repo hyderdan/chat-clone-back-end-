40b0bb087244f47848780e490c35f51ce4d04713
Hyder Danish <hyder.danish369@gmail.com>
2024-07-05 16:29:16 +0530

    done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,28 @@
 const express = require('express');
-const userROutes = express.Router();
 const bodyParser = require('body-parser');
-const userControlls = require('../controllers/usercontroller');
+const {userControll} = require('../controllers/usercontroller');
 
-userROutes.use(bodyParser.json());
-userROutes.use(bodyParser.urlencoded({ extended: true }));
-userROutes.post('/sigUp', userControlls.SignUp);
-userROutes.post('/login', userControlls.LoginUser);
-userROutes.get('/singleUser/:userId', userControlls.userDeatails);
-userROutes.post('/searchUser', userControlls.searchUser);
-userROutes.post('/AddFriend/:ID', userControlls.AddToFriendList);
-userROutes.get('/friendList/:userid', userControlls.friends);
-userROutes.post('/AddToFavourate', userControlls.AddToFavourates);
-userROutes.get('/GetFavourates/:userId', userControlls.GetFavourates);
+const initUserRoutes = (io) => {
 
+    const userROutes = express.Router();
+    const userControlls = userControll(io)
+
+    userROutes.use(bodyParser.json());
+    userROutes.use(bodyParser.urlencoded({ extended: true }));
+    userROutes.post('/sigUp', userControlls.SignUp);
+    userROutes.post('/login', userControlls.LoginUser);
+    userROutes.get('/singleUser/:userId', userControlls.userDeatails);
+    userROutes.post('/searchUser', userControlls.searchUser);
+    userROutes.post('/AddFriend/:ID', userControlls.AddToFriendList);
+    userROutes.get('/friendList/:userid', userControlls.friends);
+    userROutes.post('/AddToFavourate', userControlls.AddToFavourates);
+    userROutes.get('/GetFavourates/:userId', userControlls.GetFavourates);
+
+    return userROutes;
+
+
+}
 
 module.exports = {
-    userROutes
+    initUserRoutes
 }
